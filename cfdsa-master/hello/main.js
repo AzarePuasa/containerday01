e03c77b226beb4889a6de9cbd8de5c0a12470b34
AzarePuasa <azarepuasa@azarepuasa.com>
2019-06-26 22:44:36 +0800

    day 01 updates and add files for day 02

--- cfdsa-master/hello/main.js
+++ cfdsa-master/hello/main.js
@@ -1,12 +1,13 @@
 const { join } = require('path')
 const express = require('express')
+let ready = false;
 
 const APP_PORT = parseInt(process.argv[2]) || parseInt(process.env.APP_PORT) || 3000
 
 const app = express();
 
 app.get('/health', (req, resp) => {
-	resp.status(200).type('text/plain').end(`${new Date()}: OK`);
+	resp.status(ready? 200: 400).type('text/plain').end(`${new Date()}: OK`);
 });
 
 app.use(express.static(join(__dirname, 'public')));
